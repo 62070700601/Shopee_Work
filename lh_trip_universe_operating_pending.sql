7da066ae52dafa053106d021c13afc03f1d31c82
Khettawan Maliyaem <68046158+62070700601@users.noreply.github.com>
2022-12-01 18:27:37 +0700

    Add files via upload

--- lh_trip_universe_operating_pending.sql
+++ lh_trip_universe_operating_pending.sql
@@ -95,6 +95,82 @@ group by
     ,from_unixtime(transportation_trip_loading.ctime-3600)
     ,from_unixtime(transportation_trip_loading.mtime-3600)
 )
+,shipment_check_sender_or_reciver as
+(
+select 
+    status_name
+    ,status
+    ,shipment_id
+    -- ,time_stamp
+    ,linehaul_id
+    ,lead_rank
+    ,case
+        when status_name = 'SOC_LHTransported' and lead_rank = 'LMHub_Received' then 'receiver correct'
+        when status_name = 'SOC_LHTransporting' and lead_rank = 'LMHub_Received' then 'receiver wrong'
+        when status_name in ('SOC_LHPacked','SOC_LHPacking') and lead_rank = 'LMHub_Received' then 'sender wrong'
+        when status_name = 'SOC_LHTransported' and lead_rank = 'SOC_Received' then 'receiver correct'
+        when status_name = 'SOC_LHTransporting' and lead_rank = 'SOC_Received' then 'receiver wrong'
+        when status_name in ('SOC_LHPacked','SOC_LHPacking') and lead_rank = 'SOC_Received' then 'sender wrong'
+        when status_name = 'FMHub_LHTransported' and lead_rank = 'SOC_Received' then 'receiver correct'
+        when status_name = 'FMHub_LHTransporting' and lead_rank = 'SOC_Received' then 'receiver wrong'
+        when status_name in ('FMHub_LHPacked','FMHub_LHPacking') and lead_rank = 'SOC_Received' then 'sender wrong'
+        when status_name = 'Return_LMHub_LHTransported' and lead_rank = 'Return_SOC_Received' then 'receiver correct'
+        when status_name = 'Return_LMHub_LHTransporting' and lead_rank = 'Return_SOC_Received' then 'receiver wrong'
+        when status_name in ('Return_LMHub_LHPacked','Return_LMHub_LHPacking') and lead_rank = 'Return_SOC_Received' then 'sender wrong'
+        when status_name = 'Return_SOC_LHTransported' and lead_rank = 'Return_SOC_Received' then 'receiver correct'
+        when status_name = 'Return_SOC_LHTransporting' and lead_rank = 'Return_SOC_Received' then 'receiver wrong'
+        when status_name in ('Return_SOC_LHPacked','Return_SOC_LHPacking') and lead_rank = 'Return_SOC_Received' then 'sender wrong'
+        when status_name = 'Return_SOC_LHTransported' and lead_rank = 'Return_LMHub_Received' then 'receiver correct'
+        when status_name = 'Return_SOC_LHTransporting' and lead_rank = 'Return_LMHub_Received' then 'receiver wrong'
+        when status_name in ('Return_SOC_LHPacked','Return_SOC_LHPacking') and lead_rank = 'Return_LMHub_Received' then 'sender wrong'
+        when status_name = 'LMHub_LHTransported' and lead_rank = 'SOC_Received' then 'receiver correct'
+        when status_name = 'LMHub_LHTransporting' and lead_rank = 'SOC_Received' then 'receiver wrong'
+        when status_name in ('LMHub_LHPacked','LMHub_LHPacking') and lead_rank = 'SOC_Received' then 'sender wrong'
+        when status not in (15,47,233,56,64) and status_name not in ('SOC_LHTransported','FMHub_LHTransported','Return_LMHub_LHTransported','Return_SOC_LHTransported','LMHub_LHTransported') and lead_rank is null then 'sender wrong' 
+        when status in (15,47,233,56,64) and lead_rank is null then 'LH wrong'
+        else 'receiver wrong'
+    end as sender_or_reciver
+from
+(
+select 
+    thspx_fact_order_tracking_di_th.status_name
+    ,order_track.status
+    ,order_track.shipment_id
+    ,from_unixtime(ctime-3600) as time_stamp
+    ,TRY(cast(JSON_EXTRACT(JSON_PARSE(content), '$.linehaul_task_id') as varchar)) as linehaul_id 
+    ,lead(thspx_fact_order_tracking_di_th.status_name) over (partition by shipment_id order by from_unixtime(ctime-3600) asc) as lead_rank
+    ,row_number() over (partition by TRY(cast(JSON_EXTRACT(JSON_PARSE(content), '$.linehaul_task_id') as varchar)) order by from_unixtime(ctime-3600) desc) as rank_num
+from spx_mart.shopee_ssc_spx_track_th_db__order_tracking_tab__reg_continuous_s0_live as order_track
+left join 
+    (
+        select 
+            status
+            ,status_name
+        from thopsbi_lof.thspx_fact_order_tracking_di_th
+        group by 
+            status
+            ,status_name
+    ) as thspx_fact_order_tracking_di_th
+on order_track.status = thspx_fact_order_tracking_di_th.status
+where 
+    -- shipment_id = 'SPXTH02429567805B'
+    date(from_unixtime(ctime-3600)) between date(DATE_TRUNC('day', current_timestamp) - interval '30' day) and date(DATE_TRUNC('day', current_timestamp) - interval '0' day)
+    -- TRY(cast(JSON_EXTRACT(JSON_PARSE(content), '$.linehaul_task_id') as varchar)) = 'LT0MBM000HQ41'
+order by
+    from_unixtime(ctime-3600) asc
+)
+where 
+    status in (34,35,15,36,45,46,47,48,231,232,233,234,54,56,57,55,61,65,64,62)
+    and rank_num = 1
+    -- and lead_rank not in ('LMHub_LHTransported','SOC_LHTransported','Return_LMHub_LHTransported','Return_LMHub_LHTransporting','Return_SOC_LHTransported','FMHub_LHTransported','SOC_LHTransporting','FMHub_LHTransporting','LMHub_LHTransporting','Return_SOC_LHTransporting','SOC_LHPacked','FMHub_LHPacked','LMHub_Packed','Return_LMHub_LHPacked','Return_SOC_LHPacked','LMHub_LHPacked')
+group by 
+    status_name
+    ,status
+    ,shipment_id
+    -- ,time_stamp
+    ,linehaul_id
+    ,lead_rank
+)
 -- With out cte
 select 
     trip_number
@@ -108,7 +184,11 @@ select
     ,vehicle_number
     ,driver as driver_id
     ,operator
+    -- ,shipment_check_sender_or_reciver.sender_or_reciver
+    ,coalesce(shipment_check_sender_or_reciver.sender_or_reciver,'Unidentified') as sender_or_reciver_type
 from cte
+left join shipment_check_sender_or_reciver
+on cte.trip_number = shipment_check_sender_or_reciver.linehaul_id
 where 
     rank_num = 1
 group by 
@@ -125,5 +205,6 @@ group by
     ,vehicle_number
     ,trip_name
     ,ctime_loading_cast
+    ,coalesce(shipment_check_sender_or_reciver.sender_or_reciver,'Unidentified')
 order by
     trip_date asc
